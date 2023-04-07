7135ed1c9e58e88536feb95c35d162b44b93d367
Giacomo Della Peruta <giax.95@live.it>
2023-04-07 16:45:56 +0200

    Fix

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -33,8 +33,8 @@ public class Main {
 		u1.setCognome("Rossi");
 		u1.setNome("Mario");
 		// **Scommentare sotto per creare utente**
-		//UtenteDAO.salvaUtente(u1);
-		System.out.println("L'utente salvato è: " + UtenteDAO.findUtente(1));
+		// UtenteDAO.salvaUtente(u1);
+		// System.out.println("L'utente salvato è: " + UtenteDAO.findUtente(1));
 
 		// **Creazione nuova tessera**
 		Tessera t1 = new Tessera();
